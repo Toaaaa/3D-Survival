8092723074b723cef813a44cf624e407dba17dd6
kim minjun <k980128@naver.com>
2025-03-12 20:59:23 +0900

    feat : 테스트 NavMesh 베이크 변경.

--- Assets/01.Scripts/Monster/Monster.cs
+++ Assets/01.Scripts/Monster/Monster.cs
@@ -29,7 +29,7 @@ public class Monster : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float lostDistance = 15; // 타겟을 잃어버리는 거리.
     [SerializeField] private Vector3 originPos; // 처음 위치.
-    [SerializeField] private float AwayDistance = 35; // 처음 구역에서 최대로 멀어질 수 있는 거리. (해당 거리 이상으로 떨어지면 모든 행동을 중지하고 원래 위치로 타겟을 설정하여 돌아감.)
+    [SerializeField] private float AwayDistance = 18; // 처음 구역에서 최대로 멀어질 수 있는 거리. (해당 거리 이상으로 떨어지면 모든 행동을 중지하고 원래 위치로 타겟을 설정하여 돌아감.)
     NavMeshAgent navMeshAgent;
     Animator anim;
     State state = State.Idle;
