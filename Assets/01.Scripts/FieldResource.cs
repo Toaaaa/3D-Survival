480171f6e226838a041d723c78b2b6b7322fc26f
GoingRight <choibin1008@naver.com>
2025-03-13 11:28:03 +0900

    feat : 필드자원 리스포너 생성 및 프리팹화
    버섯과 꽃을 생성 및 재생성하는 스포너 구현 & 프리팹화

---
+++ Assets/01.Scripts/FieldResource.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldResource : ItemObject
+{
+    public Action respawn;
+    private FieldResourceSpawner spawner;
+
+    private void Start()
+    {
+        spawner = GetComponentInParent<FieldResourceSpawner>();
+        respawn = spawner.Respawn;
+    }
+
+    //public override void OnInteract()
+    //{
+    //    //아이템데이터에 넣어줌
+    //    //CharacterManager.Instance.Player.ItemData = itemData;
+    //    //액션
+    //    //CharacterManager.Instance.Player.AddItem.Invoke();
+    //    respawn?.Invoke();
+
+    //    Destroy(gameObject);
+    //}
+}
