fff4b9a3bbaaf09098e9028e9e910ffd739a5a6e
alsoox <alsoox173@gmail.com>
2025-03-17 11:16:20 +0900

    feat : player 이동 및 공격 사운드 추가

---
+++ Assets/01.Scripts/Player/PlayerSound.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    AudioSource audioSource;
+    [SerializeField] AudioClip[] footstepClips;
+    [SerializeField] AudioClip[] attackSound;
+    float footStepThreshold = 0.3f;
+    float footStepRate = 0.5f;
+    float footStepTime;
+
+    PlayerAttack attack;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        attack = GetComponent<PlayerAttack>();
+        
+    }
+
+    private void Start()
+    {
+        attack.onHit += AttackSound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(_rigidbody.velocity.y) < 0.1f)
+        {
+            if (_rigidbody.velocity.magnitude > footStepThreshold)
+            {
+                if (Time.time - footStepTime > footStepRate)
+                {
+                    footStepTime = Time.time;
+                    audioSource.PlayOneShot(footstepClips[Random.Range(0, footstepClips.Length)]);                        
+                }
+            }
+        }
+    }
+
+    public void AttackSound()
+    {
+        audioSource.PlayOneShot(attackSound[Random.Range(0, attackSound.Length)]);
+    }
+}
